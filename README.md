e4a09826ebd59e612b261de7f3195cee56600055
Troy Dawson <tdawson@redhat.com>
2019-12-10 12:48:14 -0800

    Update build-order-sort.sh script

--- README.md
+++ README.md
@@ -9,3 +9,23 @@ Plus, if you want to see what I did, go for it.
 
 NOTE: NOT FOR PRODUCTION USE - ANY OF THESE
 THESE ARE FOR PROTOTYPING VARIOUS THINGS FAST
+
+## build-order-sort.sh
+Given a list of source packages, finds all the build dependencies and
+then sorts them into the order they should be built.
+
+If there is a circular dependency, among the list of packages, it will
+stop itself and say "SOMETHING IS WRONG"  If that happens, it is up to
+you to determine the circular dependency.  It is best to start looking at
+the dependencies for the packages on the layer that first says something
+is wrong.
+
+### Examples:
+* build-order-sort.sh -v --no-order kde.list
+** This gets the dependencies, and clears the build order
+* build-order-sort.sh --no-new-deps --no-new-order kde.list
+** The attempts to set the build order
+* # Fix the first batch of circular dependencies
+* build-order-sort.sh --no-new-deps kde.list
+** Clear out the build order, and attempts to set the build orders.
+
--- build-order-sort.sh
+++ build-order-sort.sh
@@ -16,6 +16,9 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 # BINARY VARIABLES
 VERBOSE="FALSE"
+DO_DEPS="TRUE"
+NEW_ORDER="TRUE"
+DO_ORDER="TRUE"
 # LIST VARIABLES
 OLD_LIST=()
 NEW_LIST=()
@@ -32,6 +35,12 @@ usage() {
   echo "  build order based on dependencies." >&2
   echo >&2
   echo "Options:" >&2
+  echo "  --no-new-deps" >&2
+  echo "    Do not redo the deps" >&2
+  echo "  --no-new-order" >&2
+  echo "    Do not redo the order" >&2
+  echo "  --no-order" >&2
+  echo "    Do not update the order" >&2
   echo "  -v --verbose --debug" >&2
   echo "    Be verbose, for debugging" >&2
   echo "  -h, --help" >&2
@@ -58,6 +67,15 @@ do
         usage
         exit 2	
       fi
+    ;;
+    --no-new-deps)
+      export DO_DEPS="FALSE"
+    ;;
+    --no-new-order)
+      export NEW_ORDER="FALSE"
+    ;;
+    --no-order)
+      export DO_ORDER="FALSE"
     ;;
     -v | --verbose | --debug)
       export VERBOSE="TRUE"
@@ -87,76 +105,84 @@ if [ "${DATAFILE}" == "" ] ; then
 fi
 
 ## Setup
-# For now, deps are already created.  just do order
-# mkdir deps
 if [ "${VERBOSE}" == "TRUE" ] ; then
   echo "SETUP"
 fi
 mkdir -p order deps
-rm -f order/*
+if [ "${DO_DEPS}" == "TRUE" ] ; then
+  rm -f deps/*
+fi
+if [ "${NEW_ORDER}" == "TRUE" ] ; then
+  rm -f order/*
+fi
 for package in $(cat ${DATAFILE})
 do
   if [ "${VERBOSE}" == "TRUE" ] ; then
     echo "  ${package}"
   fi
-  echo "${BUFFER}" >> order/${package}
-  #dnf --enablerepo=rawhide-source repoquery --qf="%{name}" list  $(dnf --enablerepo=rawhide-source  repoquery --srpm --qf="%{sourcerpm}" --requires --resolve ${package} 2>/dev/null | sed "s/.rpm$//") >> deps/${package} 2>/dev/null
-done
-
-#Get initial order numbers
-OLD_LIST+=("$(pushd order >/dev/null ; cat * | sort -n | uniq -c | awk '{print $2 ":" $1}';popd >/dev/null)")
-DO_LOOP="TRUE"
-OLD_EQUALS_NEW="FALSE"
-while [ "${DO_LOOP}" == "TRUE" ] 
-do
-  echo "Doing a layer of ordering builds"
-  # Do a layer of ordering
-  for package in $(cat kde.list)
-  do
-    let this_order_num=$(cat order/$package)
-    for build_source in $(cat deps/$package)
-    do
-      if grep -q ^${build_source}$ kde.list ; then
-        let source_order_num=$(cat order/$build_source)
-        if [ $this_order_num -le $source_order_num ] ; then
-          let this_order_num=$source_order_num+2
-  	if [ "${VERBOSE}" == "TRUE" ] ; then
-            echo "  $package $this_order_num"
-  	fi
-        fi
-      fi
-    done
-    echo $this_order_num > order/$package
-  done
-  NEW_LIST=()
-  NEW_LIST+=("$(pushd order >/dev/null ; cat * | sort -n | uniq -c | awk '{print $2 ":" $1}'; popd   >/dev/null)")
-  count=0
-  OLD_EQUALS_NEW="TRUE"
-  for new_line in ${NEW_LIST[@]}
-  do
-    line_number="$(echo ${new_line} | cut -d':' -f1)"
-    old_line="${OLD_LIST[${count}]}"
-    let count=$count+1
-    let proper_line_number=$count*2
-    if [ ${proper_line_number} -eq ${line_number} ] ; then
-      if [ "${old_line}" == "${new_line}" ] ; then
-        echo -e "${CYAN}${new_line}${NC}"
-      else
-        echo -e "${BROWN}${new_line} <-${CYAN}${old_line}${NC}"
-        OLD_EQUALS_NEW="FALSE"
-      fi
-    else
-      echo -e "${YELLOW}${new_line} - SOMETHING IS WRONG${NC}"
-      DO_LOOP="FALSE"
-      OLD_EQUALS_NEW="FALSE"
-    fi
-  done
-  if [ "${OLD_EQUALS_NEW}" == "TRUE" ] ; then
-    DO_LOOP="FALSE"
-    echo "FINISHED"
-  else
-    OLD_LIST=()
-    OLD_LIST+=(${NEW_LIST[@]})
+  if [ "${DO_DEPS}" == "TRUE" ] ; then
+    dnf --enablerepo=rawhide-source repoquery --qf="%{name}" list  $(dnf --enablerepo=rawhide-source  repoquery --srpm --qf="%{sourcerpm}" --requires --resolve ${package} 2>/dev/null | sed "s/.rpm$//") >> deps/${package} 2>/dev/null
+  fi
+  if [ "${NEW_ORDER}" == "TRUE" ] ; then
+    echo "${BUFFER}" >> order/${package}
   fi
 done
 
+if [ "${DO_ORDER}" == "TRUE" ] ; then
+  #Get initial order numbers
+  OLD_LIST+=("$(pushd order >/dev/null ; cat * | sort -n | uniq -c | awk '{print $2 ":" $1}';popd >/dev/null)")
+  DO_LOOP="TRUE"
+  OLD_EQUALS_NEW="FALSE"
+  while [ "${DO_LOOP}" == "TRUE" ] 
+  do
+    echo "Doing a layer of ordering builds"
+    # Do a layer of ordering
+    for package in $(cat kde.list)
+    do
+      let this_order_num=$(cat order/$package)
+      for build_source in $(cat deps/$package)
+      do
+        if grep -q ^${build_source}$ kde.list ; then
+          let source_order_num=$(cat order/$build_source)
+          if [ $this_order_num -le $source_order_num ] ; then
+            let this_order_num=$source_order_num+2
+            if [ "${VERBOSE}" == "TRUE" ] ; then
+              echo "  $package $this_order_num"
+            fi
+          fi
+        fi
+      done
+      echo $this_order_num > order/$package
+    done
+    NEW_LIST=()
+    NEW_LIST+=("$(pushd order >/dev/null ; cat * | sort -n | uniq -c | awk '{print $2 ":" $1}'; popd   >/dev/null)")
+    count=0
+    OLD_EQUALS_NEW="TRUE"
+    for new_line in ${NEW_LIST[@]}
+    do
+      line_number="$(echo ${new_line} | cut -d':' -f1)"
+      old_line="${OLD_LIST[${count}]}"
+      let count=$count+1
+      let proper_line_number=$count*2
+      if [ ${proper_line_number} -eq ${line_number} ] ; then
+        if [ "${old_line}" == "${new_line}" ] ; then
+          echo -e "${CYAN}${new_line}${NC}"
+        else
+          echo -e "${BROWN}${new_line} <-${CYAN}${old_line}${NC}"
+          OLD_EQUALS_NEW="FALSE"
+        fi
+      else
+        echo -e "${YELLOW}${new_line} - SOMETHING IS WRONG${NC}"
+        DO_LOOP="FALSE"
+        OLD_EQUALS_NEW="FALSE"
+      fi
+    done
+    if [ "${OLD_EQUALS_NEW}" == "TRUE" ] ; then
+      DO_LOOP="FALSE"
+      echo "FINISHED"
+    else
+      OLD_LIST=()
+      OLD_LIST+=(${NEW_LIST[@]})
+    fi
+  done
+fi
